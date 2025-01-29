615ef0502a9750284f0bc64975a7fd7a1d593013
Lehi Rolim - Asus Vivobook <lehirolim@gmail.com>
2025-01-29 19:29:18 -0300

    fazendo botão funcionar

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,1 +1,8 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+document.querySelector('h1').textContent = 'Amigo Secreto do Lehi';
+document.querySelector('h2').textContent = 'Digite o nome dos seus amigos um  de cada vez.';
+let nomeAmigo = [];
+
+function adicionarAmigo(){
+    nomeAmigo = botao = document.querySelector('input').value;
+    console.log(nomeAmigo);
+}
